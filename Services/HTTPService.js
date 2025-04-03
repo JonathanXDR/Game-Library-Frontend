39b43aab5aa6ec1c6aac935cb7c1dbb1a72cd5a1
Jonathan Russ <jonathan.russ@bluewin.ch>
2025-04-03 08:44:29 +0200

    Add vue files

--- Services/HTTPService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-const instance = axios.create({
-  // BaseURL of every axios request
-  baseURL: 'https://app.planetscale.com/jonathan-russ/crud-app',
-});
-
-export default instance;
